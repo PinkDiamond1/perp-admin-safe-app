1c355ce4734a38753cd601e1b175e5ced7c268e9
Yuren Ju <yurenju@gmail.com>
2021-05-14 13:33:45 +0800

    fix: incorrect copy artifact pattern

--- scripts/copy-artifacts.ts
+++ scripts/copy-artifacts.ts
@@ -2,8 +2,7 @@
 import {find,cp,mkdir} from 'shelljs'
 
 const destDir = './apps/perp-admin/public/artifacts'
-const artifactPaths = find('./node_modules/@perp/contract/build/contracts/src').filter(file => file.match(/[^(dbg)]\.json$/))
-console.log(artifactPaths)
+const artifactPaths = find('./node_modules/@perp/contract/build/contracts/src').filter(file => file.match(/\.json$/) && !file.includes('dbg'))
 
 mkdir('-p', destDir)
 cp(artifactPaths, destDir)
